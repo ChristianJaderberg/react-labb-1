579b5cb898b7ead5f3da578b3ee3c4ff905ab66d
Christian Jäderberg <christianjaderberg@gmail.com>
2020-11-27 09:10:21 +0100

    Added conditional-operator

--- src/App.js
+++ src/App.js
@@ -8,33 +8,44 @@ import Go from './components/Go'
 function App() {
 
   const [name, setName] = useState('Random Person')
+  const [luckyNumber, setLuckyNumber] = useState(3)
 
   return (
     <div className="App">
       <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Ready</Link>
-            </li>
-            <li>
-              <Link to="/set">Set</Link>
-            </li>
-            <li>
-              <Link to="/go">Go</Link>
-            </li>
-          </ul>
-        </nav>
-        <label>
-          What is your name? 
-        <input
-          onChange={(event) => setName(event.target.value)}
-          value={name}
-        />
-        </label>
         <header className="App-header">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Ready</Link>
+              </li>
+              <li>
+                <Link to="/set">Set</Link>
+              </li>
+              <li>
+                <Link to="/go">Go</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <label>
+            What is your name? 
+          <input
+            onChange={(event) => setName(event.target.value)}
+            value={name}
+          />
+          </label>
+
+          <label>
+            What is your lucky number? 
+          <input
+            onChange={(event) => setLuckyNumber(event.target.value)}
+            value={luckyNumber}
+          />
+          </label>
+
           <Switch>
-            <Route exact path="/"><Ready name={name} /></Route>
+            <Route exact path="/"><Ready name={name} luckyNumber={luckyNumber} /></Route>
             <Route path="/set"><Set name={name} /></Route>
             <Route path="/go"><Go name={name} /></Route>
           </Switch>
--- src/components/Ready.js
+++ src/components/Ready.js
@@ -5,8 +5,10 @@ function Ready(props) {
       <div>
             <h2>Get ready, {props.name}!</h2>
             {props.name.length < 5 && <p>Your name has less than 5 characters!</p>}
-            {props.name.length == 5 && <p>Your name has exactly 5 characters!</p>}
+            {props.name.length === 5 && <p>Your name has exactly 5 characters!</p>}
             {props.name.length > 5 && <p>Wow, your name has more than 5 characters!!</p>}
+
+            {props.luckyNumber > 10 ? <p>Your lucky number is way off! Good luck with that...</p> : <p>Well ok, your lucky number is pretty cliche if you ask me.</p>}
       </div>
     )
 }
